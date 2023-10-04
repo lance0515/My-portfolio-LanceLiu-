75e70f32e767769518d61ff038464b7d1b5ee8dd
lance0515 <dennis051507@gmail.com>
2023-10-05 02:30:20 +0800

    new

--- pages/gallery.js
+++ pages/gallery.js
@@ -2,6 +2,18 @@
 import { Container, Heading,SimpleGrid  } from '@chakra-ui/react'
 import Section from '../components/section'
 
+export const getStaticProps = async () => {
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`
+    const data = await fetch(url)
+    const feed = await data.json()
+    console.log(feed)
+    return {
+        props: {
+            feed,
+        },
+    }
+
+}
 
 
 
@@ -42,18 +54,6 @@ export default function Gallery({ feed }) {
 
 
     )
-
-}
-export const getStaticProps = async () => {
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`
-    const data = await fetch(url)
-    const feed = await data.json()
-    console.log(feed)
-    return {
-        props: {
-            feed,
-        },
-    }
 
 }
 
