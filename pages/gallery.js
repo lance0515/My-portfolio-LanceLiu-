291dbaec355c08801a9035bead0c98ddd287fe5a
lance0515 <dennis051507@gmail.com>
2023-10-05 03:12:34 +0800

    Update gallery.js

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,60 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
-import { Container, Heading,SimpleGrid  } from '@chakra-ui/react'
-import Section from '../components/section'
+import { Container,Heading } from "@chakra-ui/react"
 
 
 
+export default function Gallery (){
 
-
-
-export default function Gallery({ feed }) {
-    console.log(feed.data)
-    const images = feed.data
-    return (
-
+    return(
         <Container>
-            <Heading as="h3" fontSize={30} mb={5} >
-               My Instagram Gallery
+            <Heading as="h3" fontSize={20}mb={4}>
+                Gallery ins
             </Heading>
-            <Section delay={0.5}>
-           
- 
-           
-                    <SimpleGrid columns={3} gap={1}>
-              
-            
-                    {images && images.map(image => (
-                       
-                        <div key={image.id}>
-                            <img 
-                            
-                            src={image.media_url}
-                            alt={image.caption} 
-                            herf={image.permalink}
-                            />   
-                            
-                            </div>
-                            
-                    ))}
-                    </SimpleGrid>
-               
-            </Section>
         </Container>
-
-
-    )
-
-}
-
-export const getStaticProps = async () => {
-    const url = `${process.env.NEXT_PUBLIC_API}${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`
-    const data = await fetch(url)
-    const feed = await data.json()
-    console.log(feed)
-    return {
-        props: {
-            feed,
-        },
-    }
-
-}
+        
+    )}
