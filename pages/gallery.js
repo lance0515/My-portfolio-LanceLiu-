bd72218ed75835485a69a6995001120e8c485c4f
lance0515 <dennis051507@gmail.com>
2023-10-05 03:06:01 +0800

    Update gallery.js

--- pages/gallery.js
+++ pages/gallery.js
@@ -47,7 +47,7 @@ export default function Gallery({ feed }) {
 }
 
 export const getStaticProps = async () => {
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`
+    const url = `${process.env.NEXT_PUBLIC_API}${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`
     const data = await fetch(url)
     const feed = await data.json()
     console.log(feed)
